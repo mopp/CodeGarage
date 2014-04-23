86f9ad4da7fbc4baa739126e75f0d2675fbfc979
mopp <ac.kxp165@gmail.com>
2014-04-23 22:45:52 +0900

    wip for list

--- c/doubly_circularly_linked_list.c
+++ c/doubly_circularly_linked_list.c
@@ -1,7 +1,7 @@
-/**
+/*
  * @file doubly_circularly_linked_list.c
- * @brief DoublyCircularlyLinkedList.
- *      tail                            head
+ * @brief This list is DoublyCircularlyLinkedList.
+ *      prev                            next
  *          first->X0->X1->last->X0->...
  *          last<-X1<-X0<-last<-X1<-...
  * @author mopp
@@ -20,39 +20,50 @@
 #include <stdlib.h>
 
 
-/* free function for list node. */
-typedef void (*free_func)(void*);
-/* comparison function for list node. */
+/*
+ * free function for list node.
+ * It is used in destruct_list().
+ */
+typedef void (*release_func)(void*);
+/*
+ * comparison function for list node.
+ * It is used in search_node().
+ */
 typedef bool (*comp_func)(void*, void*);
 /*
  * for each function for list node.
  * if return value is false, loop is abort
  */
-typedef bool (*loop_func)(void*);
+typedef bool (*for_each_func)(void*);
 
 
-struct dlinked_list_node {
-    void* data;
-    struct dlinked_list_node* head;
-    struct dlinked_list_node* tail;
+/*
+ * List node structure.
+ * It is in List structure below.
+ */
+struct list_node {
+    void* data;             /* pointer to stored data in node. */
+    struct list_node* next; /* pointer to next position node. */
+    struct list_node* prev; /* pointer to previous position node. */
 };
-typedef struct dlinked_list_node Dlinked_list_node;
+typedef struct list_node List_node;
 
 
-struct dlinked_list {
-    Dlinked_list_node* first;
-    Dlinked_list_node* last;
-    free_func free;
-    size_t size;
-    size_t data_type_size;
+/* List structure */
+struct list {
+    List_node* node;       /* start position pointer to node.
+                            * and XXX: this node is first, this node->prev is last.
+                            */
+    release_func free;     /* function for releasing allocated data. */
+    size_t size;           /* the number of node. */
+    size_t data_type_size; /* it provided by sizeof(data). */
 };
-typedef struct dlinked_list Dlinked_list;
+typedef struct list List;
 
 
 /* inisialize list */
-Dlinked_list* init_list(Dlinked_list* l, size_t size, free_func f) {
-    l->first = NULL;
-    l->last = NULL;
+List* init_list(List* l, size_t size, release_func f) {
+    l->node = NULL;
     l->free = f;
     l->size = 0;
     l->data_type_size = size;
@@ -62,11 +73,13 @@ Dlinked_list* init_list(Dlinked_list* l, size_t size, free_func f) {
 
 
 /* allocate new node and set data in it. */
-Dlinked_list_node* get_new_dlinked_list_node(Dlinked_list* l, void* data) {
-    Dlinked_list_node* n = (Dlinked_list_node*)malloc(sizeof(Dlinked_list_node));
+List_node* get_new_list_node(List* l, void* data) {
+    List_node* n = (List_node*)malloc(sizeof(List_node));
     if (n == NULL) {
         return NULL;
     }
+
+    n->next = n->prev = NULL;
 
     /* copy data to new area */
     n->data = malloc(l->data_type_size);
@@ -75,66 +88,29 @@ Dlinked_list_node* get_new_dlinked_list_node(Dlinked_list* l, void* data) {
     }
     memcpy(n->data, data, l->data_type_size);
 
-    n->head = NULL;
-    n->tail = NULL;
-
     return n;
 }
 
 
-/* all node in list will be freed. */
-void destruct_list(Dlinked_list* l) {
-    if (l->first == NULL && l->last == NULL) {
-        return;
-    }
-
-    /* select default free function or user free function. */
-    free_func f = (l->free != NULL) ? l->free : free;
-
-    if (l->first == l->last) {
-        f(l->first->data);
-        free(l->first);
-        l->first = l->last = NULL;
-    }
-
-    Dlinked_list_node* n = l->first;
-    Dlinked_list_node* t;
-    do {
-        t = n->tail;
-        f(n->data);
-        free(n);
-        n = t;
-    } while (n != l->last);
-
-    f(t->data);
-    free(t);
-
-    l->last = l->first = NULL;
-    l->size = 0;
-}
-
-
 /*
- * add new node into head of argument node.
+ * add new node into next of argument node.
+ * after execute, list is "... -> target -> new_node -> ...".
  * @return pointer to new node.
  */
-Dlinked_list_node* insert_head(Dlinked_list* l, Dlinked_list_node* target, void* data) {
+List_node* insert_next(List* l, List_node* target, void* data) {
     assert(l != NULL && target != NULL);
 
-    Dlinked_list_node* new = get_new_dlinked_list_node(l, data);
+    List_node* new = get_new_list_node(l, data);
 
-    new->head = target->head;
-    if (new->head != NULL) {
-        new->head->tail = new;
-    }
+    new->next = target->next;
+    new->next->prev = new;
 
-    new->tail = target;
-    if (new->tail != NULL) {
-        new->tail->head = new;
-    }
+    new->prev = target;
+    new->prev->next = new;
 
-    if (target == l->first) {
-        l->first = new;
+    /* set new node. */
+    if (target == l->node->prev) {
+        l->node->prev = new;
     }
 
     ++(l->size);
@@ -144,26 +120,24 @@ Dlinked_list_node* insert_head(Dlinked_list* l, Dlinked_list_node* target, void*
 
 
 /*
- * add new node into tail of argument node.
+ * add new node into prev of argument node.
+ * after execute, list is "... -> new_node -> target -> ...".
  * @return pointer to new node.
  */
-Dlinked_list_node* insert_tail(Dlinked_list* l, Dlinked_list_node* target, void* data) {
+List_node* insert_prev(List* l, List_node* target, void* data) {
     assert(l != NULL && target != NULL);
 
-    Dlinked_list_node* new = get_new_dlinked_list_node(l, data);
+    List_node* new = get_new_list_node(l, data);
 
-    new->tail = target->tail;
-    if (new->tail != NULL) {
-        new->tail->head = new;
-    }
+    new->prev = target->prev;
+    new->prev->next = new;
 
-    new->head = target;
-    if (new->head != NULL) {
-        target->tail = new;
-    }
+    new->next = target;
+    target->prev = new;
 
-    if (target == l->last) {
-        l->last = new;
+    /* set new node. */
+    if (target == l->node) {
+        l->node = new;
     }
 
     ++(l->size);
@@ -172,16 +146,17 @@ Dlinked_list_node* insert_tail(Dlinked_list* l, Dlinked_list_node* target, void*
 }
 
 
-static inline void insert_first_node(Dlinked_list* l, void* data) {
-    assert(l->first == NULL && l->last == NULL);
+/* insert node when list has NOT any node. */
+static inline void insert_first_node(List* l, void* data) {
+    assert(l->node == NULL);
 
-    l->last = l->first = get_new_dlinked_list_node(l, data);
-    l->first->head = l->first;
-    l->first->tail = l->first;
+    /* set pointer to self. */
+    l->node = get_new_list_node(l, data);
+    l->node->next = l->node->prev = l->node;
 
     ++l->size;
 
-    assert(l->first != NULL && l->last != NULL);
+    assert(l->node != NULL);
 }
 
 
@@ -189,11 +164,11 @@ static inline void insert_first_node(Dlinked_list* l, void* data) {
  * add new node before first node in list
  * @return pointer to list.
  */
-Dlinked_list* insert_first(Dlinked_list* l, void* data) {
-    if (l->first == NULL && l->last == NULL) {
+List* insert_first(List* l, void* data) {
+    if (l->node == NULL) {
         insert_first_node(l, data);
     } else {
-        insert_head(l, l->first, data);
+        insert_prev(l, l->node, data);
     }
 
     return l;
@@ -204,21 +179,23 @@ Dlinked_list* insert_first(Dlinked_list* l, void* data) {
  * add new node after last node in list
  * @return pointer to list.
  */
-Dlinked_list* insert_last(Dlinked_list* l, void* data) {
-    if (l->first == NULL && l->last == NULL) {
+List* insert_last(List* l, void* data) {
+    if (l->node == NULL) {
         insert_first_node(l, data);
     } else {
-        l->last = insert_tail(l, l->last, data);
+        insert_next(l, l->node->prev, data);
     }
 
     return l;
 }
 
 
-/* delete node in list. */
-void delete_node(Dlinked_list* l, Dlinked_list_node* target) {
-    target->head->tail = target->tail;
-    target->tail->head = target->head;
+/* delete argument node in list. */
+void delete_node(List* l, List_node* target) {
+    assert(target != NULL);
+
+    target->next->prev = target->prev;
+    target->prev->next = target->next;
 
     if (l->free != NULL) {
         l->free(target);
@@ -230,13 +207,41 @@ void delete_node(Dlinked_list* l, Dlinked_list_node* target) {
 }
 
 
+/* all node in list will be freed. */
+void destruct_list(List* l) {
+    if (l->node == NULL) {
+        /* do nothing. */
+        return;
+    }
+
+    /* select default free function or user free function. */
+    release_func f = (l->free != NULL) ? l->free : free;
+
+    List_node* n = l->node;
+    List_node* t;
+    do {
+        t = n->next;
+        f(n->data);
+        free(n);
+        n = t;
+    } while (n != l->node->prev);
+
+    f(t->data);
+    free(t);
+
+    l->node = NULL;
+    l->size = 0;
+}
+
+
 /* search node witch has argument data. */
-Dlinked_list_node* search_node(Dlinked_list* l, void* data, comp_func f) {
-    if (l->first == NULL || l->last == NULL) {
+List_node* search_node(List* l, void* data, comp_func f) {
+    if (l->node == NULL) {
         return NULL;
     }
 
-    for (Dlinked_list_node* n = l->first; n != l->last; n = n->tail) {
+    // FIXME
+    for (List_node* n = l->node; n != l->node; n = n->prev) {
         if (f(n->data, data) == true) {
             return n;
         }
@@ -246,36 +251,40 @@ Dlinked_list_node* search_node(Dlinked_list* l, void* data, comp_func f) {
 }
 
 
-Dlinked_list* list_for_each(Dlinked_list* const l, loop_func const f, bool const is_reverse) {
+/* for each loop. */
+List* list_for_each(List* const l, for_each_func const f, bool const is_reverse) {
     assert(f != NULL);
 
-    if (l->first == NULL || l->last == NULL) {
+    if (l->node == NULL) {
         assert(0);
         return l;
     }
 
-    Dlinked_list_node* n;
-    if (is_reverse == true) {
-        /* last to first loop */
-        n = l->last;
-        do {
-            if (false == f(n->data)) {
-                break;
-            }
-            n = n->head;
-        } while (n != l->first);
-    } else {
-        /* first to last loop */
-        n = l->last;
-        n = l->first;
-        do {
-            if (false == f(n->data)) {
-                break;
-            }
-            n = n->tail;
-        } while (n != l->last);
+    if (l->node == l->node->next) {
+        /* when list has only one node. */
+        f(l->node->data);
+        return l;
     }
-    f(n->data);
+
+    List_node* n;
+
+    if (is_reverse == false) {
+        if (false == f(l->node->data)) {
+            return l;
+        }
+    }
+
+    n = (is_reverse == true) ? l->node->prev : l->node->next;
+    do {
+        if (false == f(n->data)) {
+            break;
+        }
+        n = (is_reverse == true) ? n->prev : n->next;
+    } while (n != l->node);
+
+    if (is_reverse == true) {
+        f(l->node->data);
+    }
 
     return l;
 }
@@ -283,49 +292,111 @@ Dlinked_list* list_for_each(Dlinked_list* const l, loop_func const f, bool const
 
 /* ---------------------------------------------------------------------------------------------------- */
 
+#define MAX_SIZE 10
+static int test_array[MAX_SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+static int const check_size = MAX_SIZE;
 
-static void print_list(Dlinked_list const* const l) {
-    printf("-------------------------------\n");
-    printf("first          : %p\n", l->first);
-    printf("last           : %p\n", l->last);
+static void print_list(List const* const l) {
+    printf("Print List---------------------\n");
+    printf("node           : %p\n", l->node);
+    printf("data in node   : %d\n", (l->node != NULL) ? *(int*)l->node->data : -1);
     printf("data_type_size : %ld\n", l->data_type_size);
     printf("size           : %ld\n", l->size);
     printf("-------------------------------\n");
 }
 
 
-bool loop_int(void* d) {
+static inline bool echo(void* d) {
     printf("%d -> ", *(int*)d);
+    return true;
+}
+
+static bool test_loop_int_rev(void* d) {
+    static int cnt = 0;
+
+    echo(d);
+    assert(*(int*)d == test_array[cnt++ % check_size]);
 
     return true;
 }
 
 
+static bool test_loop_int(void* d) {
+    static int cnt = 0;
+
+    echo(d);
+    assert(*(int*)d == test_array[check_size - cnt++ % check_size - 1]);
+
+    return true;
+}
+
+
+static void test_destract(List* l) {
+    printf("Destruct List------------------\n");
+    destruct_list(l);
+    assert(l->size == 0 && l->node == NULL);
+    printf("Delete all node in List\n");
+    printf("-------------------------------\n");
+}
+
+
 int main(void) {
-    int const loop_cnt = 10;
-    Dlinked_list l;
+    List l;
     init_list(&l, sizeof(int), NULL);
 
     print_list(&l);
 
-    for (int i = 0; i < loop_cnt; i++) {
-        insert_first(&l, &i);
+    printf("Insert First-------------------\n");
+    for (int i = 0; i < check_size; i++) {
+        insert_first(&l, test_array + i);
     }
 
-    print_list(&l);
-
     printf("first -> ");
-    list_for_each(&l, loop_int, false);
+    list_for_each(&l, test_loop_int, false);
     puts("last");
 
     printf("last -> ");
-    list_for_each(&l, loop_int, true);
+    list_for_each(&l, test_loop_int_rev, true);
+    puts("first");
+    printf("-------------------------------\n");
+
+    test_destract(&l);
+
+    print_list(&l);
+
+    printf("Insert Last-------------------\n");
+    for (int i = 0; i < check_size; i++) {
+        insert_last(&l, test_array + i);
+    }
+
+    printf("first -> ");
+    list_for_each(&l, test_loop_int_rev, false);
+    puts("last");
+
+    printf("last -> ");
+    list_for_each(&l, test_loop_int, true);
+    puts("first");
+    printf("-------------------------------\n");
+
+    test_destract(&l);
+
+    int num = 100;
+    insert_first(&l, &num);
+    assert(*(int*)(l.node->data) == num);
+    num++;
+    insert_next(&l, l.node, &num);
+    assert(*(int*)l.node->next->data == num);
+    num++;
+
+    insert_prev(&l, l.node, &num);
+    assert(*(int*)l.node->data == num);
+    num++;
+
+    printf("last -> ");
+    list_for_each(&l, echo, false);
     puts("first");
 
-
-    printf("Destruct\n");
-    destruct_list(&l);
-    print_list(&l);
+    printf("\nAll Test Passed !\n");
 
     return 0;
 }

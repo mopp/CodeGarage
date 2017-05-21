a10783e7e7ba9b7184332977cdfa57b9ef54f2f9
mopp <hello@mopp.jp>
2017-05-21 10:24:40 +0900

    [FIX] mapping.rs

--- rust/mapping.rs
+++ rust/mapping.rs
@@ -41,29 +41,84 @@ fn main()
 
 
 mod sub {
+    // http://keens.github.io/blog/2016/02/28/rustnohigherkinded_type_trait/
+    // https://gist.github.com/14427/af90a21b917d2892eace
+
     pub trait HKT<U> {
         type C; // Current type
         type T; // Type with C swapped with U
     }
 
-
-    pub trait Functor<U> {
-        type C;
-        type T;
+    pub trait Functor<U>: HKT<U> {
         fn map<F>(&self, f: F) -> Self::T where F: Fn(&Self::C) -> U;
     }
 
+    pub trait Applicative<U>: Functor<U> {
+        fn pure_(value: U) -> Self::T where Self: HKT<U, C=U>;
+        fn seq<F>(&self, <Self as HKT<F>>::T) -> <Self as HKT<U>>::T where Self: HKT<F>, F: Fn(&<Self as HKT<F>>::C) -> U;
+    }
+
+    pub trait Monad<U>: Applicative<U> {
+        fn bind<F>(&self, F) -> Self::T where F : FnMut(&Self::C) -> Self::T;
+
+        fn return_(x: U) -> Self::T where Self: HKT<U, C=U> {
+            Self::pure_(x)
+        }
+
+        fn join<T>(&self) -> T where Self: HKT<U, T=T, C=T>, T: Clone {
+            self.bind(|x| x.clone())
+        }
+    }
 
     impl<T, U> HKT<U> for Option<T> {
         type C = T;
         type T = Option<U>;
     }
 
+    impl<T, U> Functor<U> for Option<T> {
+        fn map<F>(&self, f: F) -> Option<U> where F: Fn(&T) -> U {
+            match *self {
+                Some(ref value) => Some( f(value) ),
+                None => None,
+            }
+        }
+    }
+
+    impl<T, U> Applicative<U> for Option<T> {
+        fn pure_(value: U) -> <Self as HKT<U>>::T { Some(value) }
+
+        fn seq<F>(&self, fs: <Self as HKT<F>>::T) -> <Self as HKT<U>>::T where F: Fn(&<Self as HKT<F>>::C) -> U {
+            match *self {
+                Some(ref value) => match fs {
+                    Some(f) => Some( f(value) ),
+                    None => None,
+                },
+                None => None,
+            }
+        }
+    }
+
+    impl<T, U> Monad<U> for Option<T> {
+        fn bind<F>(&self, mut f: F) -> Option<U> where F : FnMut(&T) -> Option<U> {
+            match *self {
+                Some(ref value) => f(value),
+                None => None,
+            }
+        }
+    }
 
     pub fn test()
     {
         let to_string = |x: usize| x.to_string();
         let opt_usize: Option<usize> = Some(100);
         assert_eq!(opt_usize.map(to_string), Some("100".to_string()));
+
+        let f1: &Fn(&i32) -> i32 = &|x| x*3;
+        let f = Some(f1);
+        let n: Option<&Fn(&i32)->i32> = None;
+        assert_eq!(Option::pure_(100).seq(f), Some(300));
+        assert_eq!(Option::pure_(100).seq(n), None);
+
+        assert_eq!(Option::return_(1).bind(|&x| Some(x + 1)), Some(2));
     }
 }

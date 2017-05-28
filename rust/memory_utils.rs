34bab44536c10ce06740d24faa4a5d0d30567805
mopp <hello@mopp.jp>
2017-05-28 10:38:34 +0900

    [ADD] add example of align_of

--- rust/memory_utils.rs
+++ rust/memory_utils.rs
@@ -1,10 +1,13 @@
-use std::mem;
-use std::ptr;
+// https://doc.rust-lang.org/std/mem/index.html
+use std::{mem, ptr};
+
 
 fn main()
 {
     uninitialized_memory();
+    align_of_memory();
 }
+
 
 fn uninitialized_memory()
 {
@@ -21,4 +24,48 @@ fn uninitialized_memory()
     for elem in &mut data[..] {
         assert_eq!(*elem, 100);
     }
+}
+
+
+fn align_of_memory()
+{
+    assert_eq!(4, mem::align_of::<i32>());
+    assert_eq!(8, mem::align_of::<i64>());
+
+    struct ObjA {
+        i: i32
+    }
+
+    struct ObjB {
+        i: i32,
+        j: i32
+    }
+
+    struct ObjC {
+        i: u8
+    }
+
+    struct ObjD {
+        i: u8,
+        j: u8
+    }
+
+    struct ObjE {
+        i: u8,
+        j: u8,
+        k: i32,
+    }
+
+    struct ObjF {
+        i: u8,
+        j: u8,
+        k: u64,
+    }
+
+    assert_eq!(4, mem::align_of::<ObjA>());
+    assert_eq!(4, mem::align_of::<ObjB>());
+    assert_eq!(1, mem::align_of::<ObjC>());
+    assert_eq!(1, mem::align_of::<ObjD>());
+    assert_eq!(4, mem::align_of::<ObjE>());
+    assert_eq!(8, mem::align_of::<ObjF>());
 }

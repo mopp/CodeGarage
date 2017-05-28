ee0cb7cc21181d52054031030cf349e3b9e0491e
mopp <hello@mopp.jp>
2017-05-29 08:56:16 +0900

    [ADD] dispose_value

--- rust/memory_utils.rs
+++ rust/memory_utils.rs
@@ -6,6 +6,7 @@ fn main()
 {
     uninitialized_memory();
     align_of_memory();
+    dispose_value();
 }
 
 
@@ -68,4 +69,23 @@ fn align_of_memory()
     assert_eq!(1, mem::align_of::<ObjD>());
     assert_eq!(4, mem::align_of::<ObjE>());
     assert_eq!(8, mem::align_of::<ObjF>());
+}
+
+
+fn dispose_value()
+{
+    let v = vec![1, 2, 3];
+    drop(v);
+
+    // Error: value used here after move.
+    // println!("{:?}", v);
+
+    let mut v = vec![1, 2, 3];
+    let x = &v[0];
+
+    // explicitly drop the reference, but the borrow still exists
+    // borrowの先は解放されない
+    drop(x);
+
+    v.push(4); // error: cannot borrow `v` as mutable because it is also borrowed as immutable
 }

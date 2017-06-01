d833387a1b41d93a87a8339649ad141265d0ef33
mopp <hello@mopp.jp>
2017-06-02 00:40:01 +0900

    [ADD] mem::size_of

--- rust/memory_utils.rs
+++ rust/memory_utils.rs
@@ -1,6 +1,36 @@
 // https://doc.rust-lang.org/std/mem/index.html
 use std::{mem, ptr};
 
+struct ObjA {
+    i: i32
+}
+
+struct ObjB {
+    i: i32,
+    j: i32
+}
+
+struct ObjC {
+    i: u8
+}
+
+struct ObjD {
+    i: u8,
+    j: u8
+}
+
+struct ObjE {
+    i: u8,
+    j: u8,
+    k: i32,
+}
+
+struct ObjF {
+    i: u8,
+    j: u8,
+    k: u64,
+}
+
 
 fn main()
 {
@@ -33,37 +63,6 @@ fn align_of_memory()
 {
     assert_eq!(4, mem::align_of::<i32>());
     assert_eq!(8, mem::align_of::<i64>());
-
-    struct ObjA {
-        i: i32
-    }
-
-    struct ObjB {
-        i: i32,
-        j: i32
-    }
-
-    struct ObjC {
-        i: u8
-    }
-
-    struct ObjD {
-        i: u8,
-        j: u8
-    }
-
-    struct ObjE {
-        i: u8,
-        j: u8,
-        k: i32,
-    }
-
-    struct ObjF {
-        i: u8,
-        j: u8,
-        k: u64,
-    }
-
     assert_eq!(4, mem::align_of::<ObjA>());
     assert_eq!(4, mem::align_of::<ObjB>());
     assert_eq!(1, mem::align_of::<ObjC>());
@@ -159,4 +158,17 @@ fn replace_value()
             mem::replace(&mut self.buf, Vec::new())
         }
     }
+}
+
+
+fn size_of()
+{
+    assert_eq!(4, mem::size_of::<i32>());
+    assert_eq!(8, mem::size_of::<i64>());
+    assert_eq!(4, mem::size_of::<ObjA>());
+    assert_eq!(8, mem::size_of::<ObjB>());
+    assert_eq!(1, mem::size_of::<ObjC>());
+    assert_eq!(2, mem::size_of::<ObjD>());
+    assert_eq!(6, mem::size_of::<ObjE>());
+    assert_eq!(10, mem::size_of::<ObjF>());
 }

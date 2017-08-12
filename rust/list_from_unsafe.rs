84c7430218a152c69f140d7469ccf9f587680039
mopp <hello@mopp.jp>
2017-08-12 15:57:03 +0900

    [UP] list_from_unsafe.rs

--- rust/list_from_unsafe.rs
+++ rust/list_from_unsafe.rs
@@ -1,5 +1,6 @@
 #![feature(unique)]
 #![feature(nonzero)]
+#![feature(allocator_api, heap_api)]
 
 use std::mem;
 use std::ptr::Unique;
@@ -9,13 +10,16 @@ extern crate core;
 use core::nonzero::NonZero;
 use core::nonzero::Zeroable;
 
+
 #[derive(Debug)]
 struct MemoryRegion(usize, bool);
+
 
 struct PtrNode<T> {
     pointer: NonZero<*mut T>,
     next: Option<Unique<PtrNode<T>>>
 }
+
 
 impl<T> PtrNode<T> {
     fn new(v: *mut T) -> PtrNode<T>
@@ -47,6 +51,7 @@ impl<T> PtrNode<T> {
     }
 }
 
+
 unsafe impl<T> Zeroable for PtrNode<T> {
     fn is_zero(&self) -> bool
     {
@@ -54,9 +59,11 @@ unsafe impl<T> Zeroable for PtrNode<T> {
     }
 }
 
+
 struct List<T> {
     head: Option<Unique<PtrNode<T>>>,
 }
+
 
 impl<T> List<T> {
     fn new() -> List<T>
@@ -69,7 +76,7 @@ impl<T> List<T> {
     fn push_front(&mut self, ptr: *mut PtrNode<T>)
     {
         let unique_ptr =
-            match unsafe {Unique::new(ptr)} {
+            match Unique::new(ptr) {
                 None  => panic!("Got a null !"),
                 Some(u)  => u
             };
@@ -113,6 +120,35 @@ impl<T> List<T> {
         }
     }
 
+    fn pop_back(&mut self) -> Option<*mut PtrNode<T>>
+    {
+        fn find_prev_last_node<T>(opt: &mut Option<Unique<PtrNode<T>>>, count: usize) -> &mut Option<Unique<PtrNode<T>>>
+        {
+            if count <= 2 {
+                opt
+            } else {
+                find_prev_last_node(&mut unsafe {opt.as_mut().unwrap().as_mut()}.next, count - 1)
+            }
+        }
+
+        let cnt = self.len();
+        if cnt == 1 {
+            let r = self.head.unwrap().as_ptr();
+            self.head = None;
+            return Some(r);
+        }
+
+        match *find_prev_last_node(&mut self.head, cnt) {
+            None => None,
+            Some(uniq)  => unsafe {
+                let mut next_node = uniq.as_ptr();
+                let r = (*next_node).next;
+                (*next_node).next = None;
+                Some(r.unwrap().as_ptr())
+            }
+        }
+    }
+
     fn len(&self) -> usize
     {
         fn count<T>(node: &Option<Unique<PtrNode<T>>>, acc: usize) -> usize
@@ -127,39 +163,95 @@ impl<T> List<T> {
 }
 
 
-fn main()
-{
-    const SIZE: usize = 1024;
-    let mut memory_regions_source: [PtrNode<MemoryRegion>; SIZE];
-    unsafe {
-        let mut regions: [MemoryRegion; SIZE] = mem::uninitialized();
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::heap::{Alloc, System, Layout};
+    use std::slice;
+
+    fn allocate_objs_for_test<'a>(count: usize) -> &'a mut [PtrNode<MemoryRegion>]
+    {
+        let layout = Layout::from_size_align(count * mem::size_of::<MemoryRegion>(), mem::align_of::<MemoryRegion>()).unwrap();
+        let ptr = unsafe { System.alloc(layout) }.unwrap();
+        let mut regions: &mut [MemoryRegion] = unsafe {slice::from_raw_parts_mut(ptr as *mut MemoryRegion, count)};
 
         let mut addr = 0x0000;
         for u in &mut regions[0..] {
-            ptr::write(u, MemoryRegion(addr, true));
+            let r = MemoryRegion(addr, true);
+            unsafe { ptr::write(u, r) ;}
             addr += 0x1000;
         }
 
-        memory_regions_source = mem::uninitialized();
-        for i in 0..SIZE {
-            let r = &mut regions[i];
+        let layout = Layout::from_size_align(count * mem::size_of::<PtrNode<MemoryRegion>>(), mem::align_of::<PtrNode<MemoryRegion>>()).unwrap();
+        let ptr = unsafe { System.alloc(layout) }.unwrap();
+        let mut memory_regions_source = unsafe {slice::from_raw_parts_mut(ptr as *mut PtrNode<MemoryRegion>, count)};
+        for i in 0..count {
             let u = &mut memory_regions_source[i];
-            ptr::write(u, PtrNode::new(r as *mut _));
+            let n = PtrNode::new(&mut regions[i] as *mut _);
+            unsafe {ptr::write(u, n)};
         }
-    };
 
-    let mut list: List<MemoryRegion> = List::new();
-    assert_eq!(list.len(), 0);
-
-    for n in memory_regions_source.as_mut() {
-        list.push_front(n as *mut _);
-    }
-    assert_eq!(list.len(), SIZE);
-
-    for i in 0..SIZE {
-        let ptr = list.pop_front();
-        assert_eq!(Some(&mut memory_regions_source[SIZE - i - 1] as *mut _), ptr);
+        memory_regions_source
     }
 
-    assert_eq!(list.len(), 0);
+
+    #[test]
+    fn test_len()
+    {
+        let mut list = List::new();
+        let memory_regions_source = allocate_objs_for_test(1024);
+
+        let mut cnt = 0;
+        for m in memory_regions_source.as_mut() {
+            assert_eq!(list.len(), cnt);
+
+            list.push_back(m);
+
+            cnt += 1;
+        }
+
+        assert_eq!(list.len(), 1024);
+    }
+
+    #[test]
+    fn test_push_front()
+    {
+        let mut list = List::new();
+        let memory_regions_source = allocate_objs_for_test(1024);
+
+        for m in memory_regions_source.as_mut() {
+            list.push_front(m);
+            assert_eq!(list.head.unwrap().as_ptr(), m as *mut _);
+        }
+    }
+
+    #[test]
+    fn test_push_back()
+    {
+        let mut list = List::new();
+        let memory_regions_source = allocate_objs_for_test(1024);
+
+        for m in memory_regions_source.as_mut() {
+            list.push_back(m);
+            assert_eq!(list.head.unwrap().as_ptr(), m as *mut _);
+            list.pop_front();
+        }
+    }
+
+    #[test]
+    fn test_pop_back()
+    {
+        let mut list = List::new();
+        let memory_regions_source = allocate_objs_for_test(1024);
+
+        for m in memory_regions_source.as_mut() {
+            list.push_front(m);
+        }
+
+        for m in memory_regions_source {
+            assert_eq!(list.pop_back(), Some(m as *mut _));
+        }
+        assert_eq!(list.len(), 0);
+    }
 }

209dda8094b3bdb77fe20ebf7d2f2b66782587a0
mopp <hello@mopp.jp>
2017-08-14 00:05:05 +0900

    [ADD] list5.rs

---
+++ rust/list5.rs
@@ -0,0 +1,93 @@
+#![feature(shared)]
+#![feature(unique)]
+#![feature(allocator_api)]
+
+use std::ptr::Shared;
+use std::ptr::Unique;
+use std::mem;
+use std::ptr;
+
+
+struct LinkedList<T> {
+    head: Option<Shared<Node<T>>>,
+    tail: Option<Shared<Node<T>>>,
+    length: usize,
+}
+
+
+struct Node<T> {
+    next: Option<Shared<Node<T>>>,
+    prev: Option<Shared<Node<T>>>,
+    element: T,
+}
+
+
+impl<T> LinkedList<T> {
+    fn new() -> LinkedList<T>
+    {
+        LinkedList {
+            head: None,
+            tail: None,
+            length: 0
+        }
+    }
+
+    fn front(&self) -> Option<&T>
+    {
+        unsafe {
+            self.head.as_ref().map(|node| &node.as_ref().element)
+        }
+    }
+
+    fn front_mut(&mut self) -> Option<&mut T>
+    {
+        unsafe {
+            self.head.as_mut().map(|node| &mut node.as_mut().element)
+        }
+    }
+
+    fn back(&self) -> Option<&T>
+    {
+        unsafe {
+            self.tail.as_ref().map(|node| &node.as_ref().element)
+        }
+    }
+
+    fn back_mut(&mut self) -> Option<&mut T>
+    {
+        unsafe {
+            self.tail.as_mut().map(|node| &mut node.as_mut().element)
+        }
+    }
+
+    fn push_front(&mut self, element: *mut T)
+    {
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::heap::{Alloc, System, Layout};
+    use std::slice;
+
+    fn allocate_unique_objs<'a, T>(count: usize) -> &'a mut [T]
+    {
+        let type_size = mem::size_of::<T>();
+        let align     = mem::align_of::<T>();
+        let layout    = Layout::from_size_align(count * type_size, align).unwrap();
+        let ptr       = unsafe { System.alloc(layout) }.unwrap();
+        unsafe { slice::from_raw_parts_mut(ptr as *mut T, count) }
+    }
+
+    #[test]
+    fn test_push_front()
+    {
+        let objs = allocate_unique_objs::<usize>(1024);
+
+        let mut list = LinkedList::new();
+        list.push_front(&mut objs[0] as *mut _)
+    }
+}

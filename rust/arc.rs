811e292e20070b5b2e2bbcc3786b2441f89bc5d0
mopp <hello@mopp.jp>
2017-09-10 22:41:58 +0900

    arc.rs: add Arc<T> example

---
+++ rust/arc.rs
@@ -0,0 +1,17 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let data = Arc::new(Mutex::new(vec![1, 2, 3]));
+
+    for i in 0..3 {
+        let data = data.clone();
+        thread::spawn(move || {
+            let mut data = data.lock().unwrap();
+            data[i] += 1;
+        });
+    }
+
+    thread::sleep(Duration::from_millis(50));
+}

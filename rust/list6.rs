46ea36d2e0d1759ac0c5ad54b4228c5d76e4d59f
mopp <hello@mopp.jp>
2017-11-11 22:40:43 +0900

    list6.rs: wip

--- rust/list6.rs
+++ rust/list6.rs
@@ -6,15 +6,44 @@ use std::convert::{AsRef, AsMut};
 use std::default::Default;
 use std::ptr::{Unique, Shared};
 
+pub type Link<T> = Shared<Node<T>>;
 
-pub struct Node<T: Default> {
-    next: Option<Shared<Node<T>>>,
-    prev: Option<Shared<Node<T>>>,
-    element: T,
+
+// pub struct Node<T> {
+//     next: Option<Shared<Node<T>>>,
+//     prev: Option<Shared<Node<T>>>,
+//     element: T,
+// }
+
+
+pub trait Node<T> {
+    fn next(&self) -> Option<&Node<T>>;
+    fn prev(&self) -> Option<&Node<T>>;
+    fn element(&self) -> &T;
+    fn element_mut(&mut self) -> &mut T;
+    fn set_next(&self, Option<&Node<T>>);
+    fn set_prev(&self, Option<&Node<T>>);
+
+    fn as_ref(&self) -> &T {
+        self.element()
+    }
+
+    fn as_mut(&mut self) -> &T {
+        self.element_mut()
+    }
+
+    fn length(&self) -> usize {
+        panic!("not yet")
+    }
+
+    fn insert_next(&mut self, new_next: &mut Node<T>) {
+        self.set_next(()
+    }
+
+    fn insert_prev(new_prev: &mut Node<T>) {
+    }
 }
 
-trait Node<T> {
-    fn as_ref(&self) -> &T;
-    fn as_mut(&mut self) -> &T;
-    fn length(&self) -> usize;
+
+fn main() {
 }

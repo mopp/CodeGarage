3a1e8137b1de7a0f7ecb9fe267d69f280b4ce507
mopp <hello@mopp.jp>
2017-11-19 03:03:58 +0900

    list7.rs: up

--- rust/list7.rs
+++ rust/list7.rs
@@ -53,15 +53,27 @@ pub trait Node<T: Node<T>> {
             return;
         }
 
-        unsafe {
-            {
-                let new_next = new_next.as_mut();
-                new_next.set_next(self.next().into());
-                new_next.set_prev(self.as_shared())
-            }
-            self.next_mut().set_prev(new_next);
-            self.set_next(new_next);
+        {
+            let new_next = unsafe { new_next.as_mut() };
+            new_next.set_next(self.next().into());
+            new_next.set_prev(self.as_shared())
         }
+        self.next_mut().set_prev(new_next);
+        self.set_next(new_next);
+    }
+
+    fn insert_prev(&mut self, mut new_prev: Shared<T>) {
+        if self.as_ptr() == new_prev.as_ptr() {
+            return;
+        }
+
+        {
+            let new_prev = unsafe { new_prev.as_mut() };
+            new_prev.set_next(self.as_shared());
+            new_prev.set_prev(self.prev().into());
+        }
+        self.prev_mut().set_next(new_prev);
+        self.set_prev(new_prev);
     }
 }
 

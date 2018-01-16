bcbde72c09c0c8b8fd83cf35f58f098379e86158
mopp <hello@mopp.jp>
2018-01-16 20:58:08 +0900

    list5.rs: rustfmt

--- rust/list5.rs
+++ rust/list5.rs
@@ -1,16 +1,13 @@
 #![crate_type = "rlib"]
 #![crate_name = "list5"]
-
 #![feature(shared)]
 #![feature(unique)]
 #![cfg_attr(test, feature(allocator_api))]
 
-use std::convert::{AsRef, AsMut};
+use std::convert::{AsMut, AsRef};
 use std::default::Default;
 use std::ops::{Deref, DerefMut};
-use std::ptr::{Unique, Shared};
-
-
+use std::ptr::{Shared, Unique};
 
 /// LinkedList struct.
 pub struct LinkedList<T: Default> {
@@ -19,38 +16,33 @@ pub struct LinkedList<T: Default> {
     tail: Node<T>,
 }
 
-
 pub struct Node<T: Default> {
     next: Option<Shared<Node<T>>>,
     prev: Option<Shared<Node<T>>>,
     element: T,
 }
 
-
 impl<T: Default> LinkedList<T> {
-    pub fn new() -> LinkedList<T>
-    {
+    pub fn new() -> LinkedList<T> {
         LinkedList {
             head: Default::default(),
             tail: Default::default(),
         }
     }
 
-    pub fn len(&self) -> usize
-    {
-        let mut node =
-            if let Some(ref head) = self.head.next {
-                unsafe { head.as_ref() }
-            } else  {
-                return 0;
-            };
+    pub fn len(&self) -> usize {
+        let mut node = if let Some(ref head) = self.head.next {
+            unsafe { head.as_ref() }
+        } else {
+            return 0;
+        };
 
         let mut cnt = 1;
         loop {
             match node.next {
                 None => break,
                 Some(ref next) => {
-                    node = unsafe {next.as_ref()};
+                    node = unsafe { next.as_ref() };
                     cnt += 1;
                 }
             }
@@ -58,36 +50,33 @@ impl<T: Default> LinkedList<T> {
         cnt
     }
 
-    pub fn front(&self) -> Option<&T>
-    {
+    pub fn front(&self) -> Option<&T> {
+        unsafe { self.head.next.as_ref().map(|node| &node.as_ref().element) }
+    }
+
+    pub fn front_mut(&mut self) -> Option<&mut T> {
         unsafe {
-            self.head.next.as_ref().map(|node| &node.as_ref().element)
+            self.head
+                .next
+                .as_mut()
+                .map(|node| &mut node.as_mut().element)
         }
     }
 
-    pub fn front_mut(&mut self) -> Option<&mut T>
-    {
+    pub fn back(&self) -> Option<&T> {
+        unsafe { self.tail.prev.as_ref().map(|node| &node.as_ref().element) }
+    }
+
+    pub fn back_mut(&mut self) -> Option<&mut T> {
         unsafe {
-            self.head.next.as_mut().map(|node| &mut node.as_mut().element)
+            self.tail
+                .prev
+                .as_mut()
+                .map(|node| &mut node.as_mut().element)
         }
     }
 
-    pub fn back(&self) -> Option<&T>
-    {
-        unsafe {
-            self.tail.prev.as_ref().map(|node| &node.as_ref().element)
-        }
-    }
-
-    pub fn back_mut(&mut self) -> Option<&mut T>
-    {
-        unsafe {
-            self.tail.prev.as_mut().map(|node| &mut node.as_mut().element)
-        }
-    }
-
-    pub fn push_front(&mut self, new_node: Unique<Node<T>>)
-    {
+    pub fn push_front(&mut self, new_node: Unique<Node<T>>) {
         let mut new_shared_node = Shared::from(new_node);
 
         {
@@ -98,15 +87,14 @@ impl<T: Default> LinkedList<T> {
 
         let new_shared_node = Some(new_shared_node);
         match self.head.next {
-            None           => self.tail.prev = new_shared_node,
+            None => self.tail.prev = new_shared_node,
             Some(mut head) => unsafe { head.as_mut().prev = new_shared_node },
         }
 
         self.head.next = new_shared_node;
     }
 
-    pub fn push_back(&mut self, new_node: Unique<Node<T>>)
-    {
+    pub fn push_back(&mut self, new_node: Unique<Node<T>>) {
         let mut new_shared_node = Shared::from(new_node);
 
         {
@@ -117,22 +105,21 @@ impl<T: Default> LinkedList<T> {
 
         let new_shared_node = Some(new_shared_node);
         match self.tail.prev {
-            None  => self.head.next = new_shared_node,
-            Some(mut tail) => unsafe {tail.as_mut().next = new_shared_node},
+            None => self.head.next = new_shared_node,
+            Some(mut tail) => unsafe { tail.as_mut().next = new_shared_node },
         }
 
         self.tail.prev = new_shared_node;
     }
 
-    pub fn pop_front(&mut self) -> Option<Unique<Node<T>>>
-    {
+    pub fn pop_front(&mut self) -> Option<Unique<Node<T>>> {
         match self.head.next {
-            None       => None,
+            None => None,
             Some(head) => {
                 self.head.next = unsafe { head.as_ref().next };
 
                 match self.head.next {
-                    None               => self.tail.prev = None,
+                    None => self.tail.prev = None,
                     Some(mut new_head) => unsafe { new_head.as_mut().prev = None },
                 }
 
@@ -141,15 +128,14 @@ impl<T: Default> LinkedList<T> {
         }
     }
 
-    pub fn pop_back(&mut self) -> Option<Unique<Node<T>>>
-    {
+    pub fn pop_back(&mut self) -> Option<Unique<Node<T>>> {
         match self.tail.prev {
-            None       => None,
+            None => None,
             Some(tail) => {
                 self.tail.prev = unsafe { tail.as_ref().prev };
 
                 match self.tail.prev {
-                    None               => self.head.next = None,
+                    None => self.head.next = None,
                     Some(mut new_tail) => unsafe { new_tail.as_mut().next = None },
                 }
 
@@ -159,10 +145,8 @@ impl<T: Default> LinkedList<T> {
     }
 }
 
-
 impl<T: Default> Default for Node<T> {
-    fn default() -> Node<T>
-    {
+    fn default() -> Node<T> {
         Node {
             next: None,
             prev: None,
@@ -171,10 +155,8 @@ impl<T: Default> Default for Node<T> {
     }
 }
 
-
 impl<T: Default> Node<T> {
-    pub fn detach(&mut self)
-    {
+    pub fn detach(&mut self) {
         if let Some(mut next) = self.next {
             let next = unsafe { next.as_mut() };
             next.prev = self.prev;
@@ -190,18 +172,14 @@ impl<T: Default> Node<T> {
     }
 }
 
-
 impl<T: Default> AsRef<T> for Node<T> {
-    fn as_ref(&self) -> &T
-    {
+    fn as_ref(&self) -> &T {
         &self.element
     }
 }
 
-
 impl<T: Default> AsMut<T> for Node<T> {
-    fn as_mut(&mut self) -> &mut T
-    {
+    fn as_mut(&mut self) -> &mut T {
         &mut self.element
     }
 }
@@ -214,66 +192,63 @@ impl<T: Default> Deref for Node<T> {
     }
 }
 
-
 impl<T: Default> DerefMut for Node<T> {
     fn deref_mut(&mut self) -> &mut T {
         &mut self.element
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::{LinkedList, Node};
 
-    use std::heap::{Alloc, System, Layout};
+    use std::heap::{Alloc, Layout, System};
     use std::mem;
     use std::slice;
     use std::ptr::Unique;
 
-    fn allocate_node_objs<'a, T>(count: usize) -> &'a mut [T] where T: Default
+    fn allocate_node_objs<'a, T>(count: usize) -> &'a mut [T]
+    where
+        T: Default,
     {
         let type_size = mem::size_of::<T>();
-        let align     = mem::align_of::<T>();
-        let layout    = Layout::from_size_align(count * type_size, align).unwrap();
-        let ptr       = unsafe { System.alloc(layout) }.unwrap();
+        let align = mem::align_of::<T>();
+        let layout = Layout::from_size_align(count * type_size, align).unwrap();
+        let ptr = unsafe { System.alloc(layout) }.unwrap();
         unsafe { slice::from_raw_parts_mut(ptr as *mut T, count) }
     }
 
     #[test]
-    fn test_push_front()
-    {
+    fn test_push_front() {
         let objs = allocate_node_objs::<Node<usize>>(1024);
 
         let mut list = LinkedList::new();
-        list.push_front(unsafe {Unique::new_unchecked(&mut objs[0])});
+        list.push_front(unsafe { Unique::new_unchecked(&mut objs[0]) });
         assert_eq!(list.len(), 1);
         assert_eq!(list.back(), Some(&0usize));
         assert_eq!(list.front(), Some(&0usize));
     }
 
     #[test]
-    fn test_push_back()
-    {
+    fn test_push_back() {
         let objs = allocate_node_objs::<Node<usize>>(1024);
 
         let mut list = LinkedList::new();
-        list.push_back(unsafe {Unique::new_unchecked(&mut objs[1])});
+        list.push_back(unsafe { Unique::new_unchecked(&mut objs[1]) });
         assert_eq!(list.len(), 1);
         assert_eq!(list.back(), Some(&0usize));
         assert_eq!(list.front(), Some(&0usize));
     }
 
     #[test]
-    fn test_pop_front()
-    {
+    fn test_pop_front() {
         let objs = allocate_node_objs::<Node<usize>>(128);
 
         let mut list = LinkedList::new();
         for (i, o) in objs.iter_mut().enumerate() {
             o.element = i;
 
-            list.push_front(unsafe {Unique::new_unchecked(o)});
+            list.push_front(unsafe { Unique::new_unchecked(o) });
         }
 
         assert_eq!(list.len(), objs.len());
@@ -282,24 +257,23 @@ mod tests {
 
         for i in (0..objs.len()).rev() {
             match list.pop_front() {
-                None       => panic!("error"),
+                None => panic!("error"),
                 Some(node) => {
-                    assert_eq!(i, unsafe {node.as_ref().element});
+                    assert_eq!(i, unsafe { node.as_ref().element });
                 }
             }
         }
     }
 
     #[test]
-    fn test_accessors()
-    {
+    fn test_accessors() {
         let objs = allocate_node_objs::<Node<usize>>(128);
 
         let mut list = LinkedList::new();
         for (i, o) in objs.iter_mut().enumerate() {
             o.element = i;
 
-            list.push_front(unsafe {Unique::new_unchecked(o)});
+            list.push_front(unsafe { Unique::new_unchecked(o) });
         }
 
         assert_eq!(list.len(), objs.len());
@@ -309,16 +283,19 @@ mod tests {
         *list.front_mut().unwrap() = 10;
         assert_eq!(list.front(), Some(&10));
         match list.pop_front() {
-            None  => panic!("error"),
+            None => panic!("error"),
             Some(node) => {
-                assert_eq!(unsafe {node.as_ref().element}, 10);
+                assert_eq!(unsafe { node.as_ref().element }, 10);
             }
         }
         assert_eq!(list.len(), objs.len() - 2);
 
         objs[1].detach();
         assert_eq!(list.len(), objs.len() - 3);
-        assert_eq!(list.head.next.unwrap().as_ptr(), &mut objs[128 - 2] as *mut _);
+        assert_eq!(
+            list.head.next.unwrap().as_ptr(),
+            &mut objs[128 - 2] as *mut _
+        );
         assert_eq!(list.head.prev.is_none(), true);
         assert_eq!(list.tail.next.is_none(), true);
         assert_eq!(list.tail.prev.unwrap().as_ptr(), &mut objs[1] as *mut _);
@@ -329,21 +306,20 @@ mod tests {
 
     struct Frame {
         order: u8,
-        is_alloc: bool
+        is_alloc: bool,
     }
 
     impl Default for Frame {
         fn default() -> Self {
             Frame {
                 order: 0,
-                is_alloc: false
+                is_alloc: false,
             }
         }
     }
 
     #[test]
-    fn test_usage()
-    {
+    fn test_usage() {
         let objs = allocate_node_objs::<Node<Frame>>(128);
 
         let mut list = LinkedList::new();
@@ -351,12 +327,12 @@ mod tests {
             o.order = 0;
             o.is_alloc = false;
 
-            list.push_front(unsafe {Unique::new_unchecked(o)});
+            list.push_front(unsafe { Unique::new_unchecked(o) });
         }
 
         match list.front() {
             Some(frame) => assert_eq!(frame.order, 0),
-            None => panic!("error")
+            None => panic!("error"),
         }
     }
 }

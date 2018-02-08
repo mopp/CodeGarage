0df70913706d9f033685372fe43ff1e91fabbf89
mopp <hello@mopp.jp>
2018-02-08 21:09:43 +0900

    buddy_system3: up

--- rust/buddy_system3.rs
+++ rust/buddy_system3.rs
@@ -1,11 +1,15 @@
 /// It manages type T in 2^N (where N is order)
+/// it is better to make T phantom type ?
+/// in the case it just manages the indices.
 trait BuddyManager<T> {
-    fn init(ObjectMapper<usize, *mut T>) -> BuddyManager<T> where Self: Sized;
+    fn new(ObjectMapper<usize, *mut T>) -> BuddyManager<T> where Self: Sized;
     fn allocate(&mut self, usize) -> Option<T>;
     fn free(&mut self, T);
 }
 
+/// keep target objects to manage.
 trait ObjectMapper<T, U> {
+    fn new(*mut U) -> ObjectMapper<T, U> where Self: Sized;
     fn to(&mut self, T) -> U;
     fn from(&mut self, U) -> T;
 }

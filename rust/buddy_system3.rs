30e62bfbcbed4ddc487c85aded70ee50dd83dee3
mopp <hello@mopp.jp>
2018-02-17 17:01:21 +0900

    buddy_system3.rs: wip

--- rust/buddy_system3.rs
+++ rust/buddy_system3.rs
@@ -1,10 +1,26 @@
+#![cfg_attr(test, feature(allocator_api))]
+#![feature(offset_to)]
+#![feature(unique)]
+
+extern crate list5;
+
+use std::marker::PhantomData;
+use list5::LinkedList;
+use list5::Node;
+
+const MAX_ORDER: usize = 15;
+
 /// It manages type T in 2^N (where N is order)
 /// it is better to make T phantom type ?
 /// in the case it just manages the indices.
-trait BuddyManager<T> {
-    fn new(ObjectMapper<usize, *mut T>) -> BuddyManager<T> where Self: Sized;
-    fn allocate(&mut self, usize) -> Option<T>;
-    fn free(&mut self, T);
+// trait BuddyManager<T> {
+//     fn new(count_object: usize) -> BuddyManager<T> where Self: Sized;
+//     fn allocate(&mut self, usize) -> Option<T>;
+//     fn free(&mut self, T);
+// }
+
+struct BuddyManager<T> {
+    used_flags: [bool]
 }
 
 /// keep target objects to manage.
@@ -13,6 +29,9 @@ trait ObjectMapper<T, U> {
     fn to(&mut self, T) -> U;
     fn from(&mut self, U) -> T;
 }
+
+
+
 
 
 #[cfg(test)]

d3344f844b7adb4289e7c2cbb55f9ddd65e141e1
mopp <hello@mopp.jp>
2018-04-19 22:29:55 +0900

    list9.rs: wip

--- rust/list9.rs
+++ rust/list9.rs
@@ -1,20 +1,40 @@
 #![feature(unique)]
 #![feature(ptr_internals)]
+#![cfg_attr(test, feature(allocator_api))]
 
+// use std::ops::{Deref, DerefMut};
 use std::ptr::{NonNull, Unique};
-use std::marker::PhantomData;
 
-type Link<T> = NonNull<Anchor<T>>;
-
-#[repr(C)]
 #[derive(Debug)]
-struct LinkedList<T> {
-    head: Option<Link<T>>,
-    tail: Option<Link<T>>,
+struct Anchor<T> {
+    next: Option<NonNull<Node<T>>>,
+    prev: Option<NonNull<Node<T>>>,
+}
+
+impl<T> Anchor<T> {
+    pub fn new() -> Anchor<T> {
+        Anchor {
+            next: None,
+            prev: None,
+        }
+    }
+}
+
+trait Node<T: Node<T>> {
+    fn anchor(&self) -> &Anchor<T>;
+    fn anchor_mut(&mut self) -> &mut Anchor<T>;
+    fn extract(&self) -> &T;
+    fn extract_mut(&mut self) -> &mut T;
+}
+
+#[derive(Debug)]
+struct LinkedList<T: Node<T>> {
+    head: Option<NonNull<T>>,
+    tail: Option<NonNull<T>>,
     length: usize,
 }
 
-impl<T> LinkedList<T> {
+impl<T: Node<T>> LinkedList<T> {
     pub fn new() -> LinkedList<T> {
         LinkedList {
             head: None,
@@ -23,80 +43,133 @@ impl<T> LinkedList<T> {
         }
     }
 
-    pub fn push_head(&mut self, node: Link<T>) {
-        self.head = Some(node);
+    pub fn head(&self) -> Option<&T> {
+        self.head.map(|head| unsafe { &*head.as_ptr() })
     }
 
-    pub fn pop_head(&mut self) -> Option<Link<T>> {
-        self.head
-    }
-}
-
-#[repr(C)]
-#[derive(Debug)]
-struct Anchor<T> {
-    next: Option<Link<T>>,
-    prev: Option<Link<T>>,
-    phantom: PhantomData<T>,
-}
-
-impl<T> Anchor<T> {
-    pub fn new() -> Anchor<T> {
-        Anchor {
-            next: None,
-            prev: None,
-            phantom: PhantomData
-        }
+    pub fn head_mut(&mut self) -> Option<&mut T> {
+        self.head.map(|head| unsafe { &mut *head.as_ptr() })
     }
 
-    unsafe fn restore(&mut self) -> &mut T {
-        std::mem::transmute::<&mut Self, &mut T>(self)
+    pub fn tail(&self) -> Option<&T> {
+        self.tail.map(|tail| unsafe { &*tail.as_ptr() })
+    }
+
+    pub fn tail_mut(&mut self) -> Option<&mut T> {
+        self.tail.map(|tail| unsafe { &mut *tail.as_ptr() })
+    }
+
+    pub fn push_head(&mut self, node: Unique<T>) {
+        self.head = Some(NonNull::from(node));
+    }
+
+    pub fn pop_head(&mut self) -> Option<Unique<T>> {
+        self.head.map(|head| head.into())
     }
 }
-
-#[repr(C)]
-#[derive(Debug)]
-struct SampleNode {
-    anchor: Anchor<SampleNode>,
-    order: usize
-}
-
-impl SampleNode {
-    pub fn new() -> SampleNode {
-        SampleNode {
-            anchor: Anchor::new(),
-            order: 0
-        }
-    }
-}
-
-fn main() {
-    let list = LinkedList::<SampleNode>::new();
-    list.push_head()
-    // let node1 = SampleNode::new();
-    // let node2 = SampleNode::new();
-    //
-    // list.push_head(node1.anchor.to_nonnull);
-    // list.push_head(node2.anchor.to_nonnull);
-    //
-    // let nonnull_anchor = list.pop_head();
-    // let node1_1 = nonnull_anchor.as_ptr()
-
-    // let addr = 0x1312312;
-    // let obj: *mut SampleNode = addr as *mut _;
-    // let nonnull = NonNull::new(obj);
-    // println!("{:?}", nonnull.as_ref());
-    // let n = nonnull.unwrap();
-    // println!("{:?}", unsafe {n.as_ref()});
-}
-
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::heap::{Alloc, System, Layout};
+    use std::mem;
+
+    fn allocate_nodes<T>(count: usize) -> *mut T {
+        let type_size = mem::size_of::<T>();
+        let align     = mem::align_of::<T>();
+        let layout    = Layout::from_size_align(count * type_size, align).unwrap();
+        let ptr = unsafe { System.alloc(layout) }.unwrap();
+
+        ptr.as_ptr() as *mut _
+    }
+
+    #[repr(C)]
+    #[derive(Debug)]
+    struct Object {
+        anchor: Anchor<Object>,
+        order: usize,
+        hoge: usize,
+        huga: usize,
+    }
+
+    // TODO: use macro or custom derive.
+    impl Node<Object> for Object {
+        fn anchor(&self) -> &Anchor<Object> {
+            &self.anchor
+        }
+
+        fn anchor_mut(&mut self) -> &mut Anchor<Object> {
+            &mut self.anchor
+        }
+
+        fn extract(&self) -> &Object {
+            self
+        }
+
+        fn extract_mut(&mut self) -> &mut Object {
+            self
+        }
+    }
+
+    impl Object {
+        pub fn new() -> Object {
+            Object {
+                anchor: Anchor::new(),
+                order: 0,
+                hoge: 1,
+                huga: 2,
+            }
+        }
+    }
+
+    // #[test]
+    fn main() {
+        let mut list1 = LinkedList::<Object>::new();
+        let mut list2 = LinkedList::<Object>::new();
+
+        const SIZE: usize = 8;
+        let nodes = allocate_nodes::<Object>(SIZE);
+
+        unsafe {
+        println!("{:?}", nodes.offset(0).as_ref());
+        list1.push_head(Unique::new_unchecked(nodes.offset(0)));
+        list1.push_head(Unique::new_unchecked(nodes.offset(1)));
+        }
+
+        let cnt = 2;
+        while cnt != 0 {
+            if let Some(mut n) = list1.pop_head() {
+                // Use note got.
+                unsafe {
+                    n.as_mut().hoge = 10;
+                    n.as_mut().huga = 7;
+                    println!("{:?}", n);
+                    println!("{:?}", n.as_mut());
+                    list2.push_head(n)
+                }
+            }
+            break
+        }
+    }
 
     #[test]
-    fn ok() {
+    fn push_head() {
+        let mut list1 = LinkedList::<Object>::new();
+
+        assert_eq!(false, list1.head().is_some());
+
+        const SIZE: usize = 8;
+        let nodes = allocate_nodes::<Object>(SIZE);
+
+        list1.push_head(unsafe { Unique::new_unchecked(nodes.offset(0)) });
+
+        assert_eq!(true, list1.head().is_some());
+        if let Some(head) = list1.head_mut() {
+            head.hoge = 10;
+            assert_eq!(10, unsafe {(*nodes.offset(0)).hoge});
+        } else {
+            panic!("error");
+        }
     }
 }
